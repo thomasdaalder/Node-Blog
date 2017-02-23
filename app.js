a7a0c7e4c8fb13ba285215b7ba46a8d5ef974a87
Thomas Daalder <thomasdaalder@gmail.com>
2017-02-23 12:42:24 +0100

    settup up database

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+const pg = require('pg');
+const db = require(__dirname + '/models/db.js')
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
---
+++ models/db.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+const connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/nodeblog';
+const db = new Sequelize(connectionString);
+
+// Defining users model
+const User = db.define('user', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+})
+
+// Defining posts model
+const Post = db.define('post', {
+  title: Sequelize.STRING,
+  body: Sequelize.STRING,
+  date: Sequelize.DATE
+})
+
+// Defining users model
+const Comment = db.define('comment', {
+  username: Sequelize.STRING,
+  body: Sequelize.STRING
+})
+
+// User.hasMany(Post);
+
+db.sync({
+	force: true,
+})
+.then(function(someParameter){
+    const oneUser = {
+        username: "kevin",
+        password: "hackme",
+    }
+    User.create(oneUser)
+})
+.catch( (error) => console.log(error) );
+
+module.exports = {
+  db: db,
+}
